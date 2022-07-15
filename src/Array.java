dbb025c6cfcbfd03da51b9840656186eda2ad5f7
dmiclea <david.miclea@capgemini.com>
2022-07-15 16:02:56 +0300

    Added insertAtIndex method

--- src/Array.java
+++ src/Array.java
@@ -7,14 +7,32 @@ public class Array {
     }
 
     public void insert(int item) {
+        checkIfResizeIsNeeded();
+        items[count++] = item;
+    }
+
+    private void checkIfResizeIsNeeded() {
         if (items.length == count) {
             int[] newItems = new int[count * 2];
             for (int i = 0; i < count; i++)
                 newItems[i] = items[i];
             items = newItems;
         }
-        items[count++] = item;
     }
+
+    public void insertAtIndex (int item, int index){
+        if(index < 0 || index >= count)
+            throw new IndexOutOfBoundsException();
+
+        checkIfResizeIsNeeded();
+        for(int i = count - 1; i >= index;i--)
+            items[i + 1] = items[i];
+
+        for (int i = 0; i < count; i++)
+            items[index] = item;
+       count++;
+    }
+
 
     public void removeAtIndex(int index) {
         if (index < 0 || index >= count)
@@ -32,9 +50,24 @@ public class Array {
         return -1;
     }
 
+    public void maxNumber(){
+        int nrMax = items[0];
+        for (int i = 0; i < count; i++){
+            if(items[i] > nrMax)
+                nrMax = items[i];
+        }
+        System.out.println(nrMax);
+    }
+
+    public void reverse() {
+        for(int i = count - 1; i>=0; --i){
+            System.out.println(items[i]);
+        }
+    }
+
     public void print() {
         for (int i = 0; i < count; i++) {
-            System.out.println( items[i] );
+            System.out.println(items[i]);
         }
     }
 }
